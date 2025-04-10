ae806712f68f77b76c7459700d68cb9ccf083edc
Mushi666 <mohtashim1233@gmail.com>
2025-04-10 19:26:57 +0500

    three remanining pos

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,26 +4,35 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { forDatabasePostgresAsyncConfig } from './config/database';
 import { adminjwtStrategy } from './providers/strategy/admin.strategy';
 import { userjwtStrategy } from './providers/strategy/user.strategy';
-import { authModule } from './modules/auth/auth.module';
-import { jwtModule } from './modules/jwt/jwt.module';
-import { playerModule } from './modules/player/player.module';
-import { userModule } from './modules/user/user.module';
-import { stripeModule } from './modules/stripe/stripe.module';
-import { mailModule } from './modules/mail/mail.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { JwtModule } from './modules/jwt/jwt.module';
+import { PlayerModule } from './modules/player/player.module';
+import { UserModule } from './modules/user/user.module';
+import { StripeModule } from './modules/stripe/stripe.module';
+import { MailModule } from './modules/mail/mail.module';
 import { OtpModule } from './modules/otp/otp.module';
 import { CloudinaryModule } from './modules/cloudinary/cloudinary.module';
+import { BullModule } from '@nestjs/bull';
+import { BullModel } from './modules/bull/bull.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }),
   TypeOrmModule.forRootAsync(forDatabasePostgresAsyncConfig),
-    authModule,
-    jwtModule,
-    playerModule,
-    userModule,
-    stripeModule,
-    mailModule,
+  BullModule.forRoot({
+    redis: {
+      connectionName: 'localhost',
+      host: '6979'
+    }
+  }),
+    AuthModule,
+    JwtModule,
+    PlayerModule,
+    UserModule,
+    StripeModule,
+    MailModule,
     OtpModule,
-    CloudinaryModule
+    CloudinaryModule,
+    BullModel
   ],
   providers: [adminjwtStrategy, userjwtStrategy],
 })
