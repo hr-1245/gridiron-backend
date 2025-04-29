4516344668e2408fdcd80262d3cc32481d806b8b
Mushi666 <mohtashim1233@gmail.com>
2025-04-29 20:12:30 +0500

    Swagger and CI/CD testing

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import * as basicAuth from 'express-basic-auth'; 
 import { getQueueToken } from '@nestjs/bull';
 import { ExpressAdapter } from '@bull-board/express';
 import { createBullBoard } from '@bull-board/api';
@@ -14,6 +15,16 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
+    }),
+  );
+
+  // Basic authentication middleware
+  app.use(
+    basicAuth({
+      users: {
+        'admin': 'mushi1264273',
+      },
+      challenge: true,
     }),
   );
 
