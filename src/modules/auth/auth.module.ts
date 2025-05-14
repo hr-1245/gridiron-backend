3039e5dec1a837724d9e4c26b813b9ccf14cb893
Mushi666 <mohtashim1233@gmail.com>
2025-05-14 11:00:57 -0700

    Feat added

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -9,10 +9,11 @@ import { adminauthController } from "./controllers/adminAuth.controller";
 import { JwtModule } from "../jwt/jwt.module";
 import { MailModule } from "../mail/mail.module";
 import { OtpModule } from "../otp/otp.module";
+import { discountConfigEntity, userPlanEntity } from "../user/entity/userPlan.entity";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([userEntity, adminauthEntity]), 
+    TypeOrmModule.forFeature([userEntity, adminauthEntity, userPlanEntity, discountConfigEntity]),
     JwtModule,
     MailModule,
     OtpModule,
@@ -20,4 +21,4 @@ import { OtpModule } from "../otp/otp.module";
   controllers: [userauthController, adminauthController],
   providers: [userAuthService, adminauthService],
 })
-export class AuthModule {}
+export class AuthModule { }
